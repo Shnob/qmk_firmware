2786b4e329a8a9d076b32256a1b77999be3aabc2
Shnob <jake.dombnz@gmail.com>
2024-09-30 18:29:58 +1300

    Added underglow to keyboard

--- keyboards/lily58/keymaps/shnob/rules.mk
+++ keyboards/lily58/keymaps/shnob/rules.mk
@@ -6,9 +6,9 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = yes           # OLED display
+OLED_ENABLE = no # yes      # OLED display
 SPLIT_TRANSPORT = yes       # From https://github.com/kata0510/Lily58/issues/10#issuecomment-570319057
 ENCODER_ENABLE = yes
 MOUSEKEY_ENABLE = yes
@@ -17,10 +17,10 @@ KEY_LOCK_ENABLE = yes
 LTO_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader_shnob.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
+#SRC +=  ./lib/layer_state_reader_shnob.c \
+#        ./lib/logo_reader.c \
+#        ./lib/keylogger.c \
+		#./lib/rgb_state_reader.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
         # ./lib/timelogger.c \
